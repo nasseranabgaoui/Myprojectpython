fdd8ccb26ea307e0682d10cb83d2cfef63ff27b6
Nassera <nasseranabgaoui@gmail.com>
2025-09-07 13:49:07 +0200

    Initial commit

---
+++ test.py
@@ -0,0 +1,14 @@
+def main():
+   # creation d'une varibale 'username' ayant pour valeur le mot Nassera
+   username = "Nassera"
+   # creation d'une variable 'age' ayant pour valeur 23
+   age = 23
+   print (username, age)
+   age = 24
+   print (age)
+   age = age + 1
+   print (age)
+
+
+if __name__ == '__main__':
+    main ()
